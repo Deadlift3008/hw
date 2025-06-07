b8f03805e900e9b651c0bab1be537d290e9339cf
deadlift3008 <deadlift3008@gmail.com>
2025-06-07 13:44:13 +0300

    hw1: first try

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
+func Reverse(str string) string {
+	reversed := []rune(str)
+	stringLength := len(str)
+
+	for i, letter := range str {
+		reversed[stringLength - i - 1] = letter
+	}
+
+	return string(reversed)
+}
+
 func main() {
-	// Place your code here.
+	startString := "Hello, OTUS!"
+
+	fmt.Println(Reverse(startString))
 }
