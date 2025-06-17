77af65feb3f698af31e53a8c278d4ee12c59d920
deadlift3008 <deadlift3008@gmail.com>
2025-06-18 01:12:56 +0300

    hw04: finished list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -19,12 +19,113 @@ type ListItem struct {
 type list struct {
 	firstElem *ListItem
 	lastElem  *ListItem
-	elemDict  map[*ListItem]*ListItem
 	length    int
 }
 
 func (l *list) Len() int {
 	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.firstElem
+}
+
+func (l *list) Back() *ListItem {
+	return l.lastElem
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	l.length++
+	if l.firstElem == nil {
+		l.firstElem = &ListItem{
+			Value: v,
+			Next:  nil,
+			Prev:  nil,
+		}
+
+		l.lastElem = l.firstElem
+
+		return l.firstElem
+	}
+
+	newFirstElem := &ListItem{
+		Value: v,
+		Next:  l.firstElem,
+		Prev:  nil,
+	}
+
+	l.firstElem.Prev = newFirstElem
+	l.firstElem = newFirstElem
+
+	return l.firstElem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	l.length++
+
+	if l.lastElem == nil {
+		l.firstElem = &ListItem{
+			Value: v,
+			Next:  nil,
+			Prev:  nil,
+		}
+
+		l.lastElem = l.firstElem
+
+		return l.firstElem
+	}
+
+	newLastElem := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  l.lastElem,
+	}
+
+	l.lastElem.Next = newLastElem
+	l.lastElem = newLastElem
+
+	return l.lastElem
+}
+
+func (l *list) Remove(i *ListItem) {
+	prev := i.Prev
+	next := i.Next
+
+	if prev != nil {
+		prev.Next = next
+	} else {
+		l.firstElem = next
+	}
+
+	if next != nil {
+		next.Prev = prev
+	} else {
+		l.lastElem = prev
+	}
+
+	l.length--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	prev := i.Prev
+	next := i.Next
+
+	if prev != nil {
+		prev.Next = next
+	} else {
+		l.firstElem = next
+	}
+
+	if next != nil {
+		next.Prev = prev
+	} else {
+		l.lastElem = prev
+	}
+
+	l.firstElem.Prev = i
+	i.Prev = nil
+	i.Next = l.firstElem
+	l.firstElem = i
 }
 
 func NewList() List {
