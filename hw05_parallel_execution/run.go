02273fa758aef2f632efb5a02543682188314505
deadlift3008 <deadlift3008@gmail.com>
2025-06-29 04:00:17 +0300

    hw05: fix data race

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -19,40 +19,51 @@ type SafeWorkerManager struct {
 	exceedErrorLimit bool
 }
 
-func (s *SafeWorkerManager) IncreaseErrorCount() {
+func (s *SafeWorkerManager) IncreaseErrorCount() bool {
 	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.currentErrors++
-	s.mu.Unlock()
-}
-
-func (s *SafeWorkerManager) takeNextTaskIndex() int {
-	s.mu.Lock()
-	s.currentTaskIndex++
-	s.mu.Unlock()
-	return s.currentTaskIndex
-}
-
-func (s *SafeWorkerManager) handle(i *int) {
-	err := s.tasks[*i]()
-
-	if err != nil {
-		s.IncreaseErrorCount()
-	}
 
 	if s.currentErrors >= s.errorLimit {
-		s.wg.Done()
 		s.exceedErrorLimit = true
-		return
+		return true
 	}
 
-	nextTaskIndex := s.takeNextTaskIndex()
+	return false
+}
 
-	if nextTaskIndex > len(s.tasks)-1 {
+func (s *SafeWorkerManager) takeNextTask() (Task, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.currentTaskIndex++
+
+	if s.currentTaskIndex > len(s.tasks)-1 {
+		return nil, false
+	}
+
+	return s.tasks[s.currentTaskIndex], true
+}
+
+func (s *SafeWorkerManager) handle(task *Task) {
+	err := (*task)()
+	if err != nil {
+		exceedErrorLimit := s.IncreaseErrorCount()
+
+		if exceedErrorLimit {
+			s.wg.Done()
+			return
+		}
+	}
+
+	nextTask, exist := s.takeNextTask()
+
+	if !exist {
 		s.wg.Done()
 		return
 	}
 
-	s.handle(&nextTaskIndex)
+	s.handle(&nextTask)
 }
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
@@ -61,7 +72,13 @@ func Run(tasks []Task, workerCount, errorLimit int) error {
 		return nil
 	}
 
-	workerManager := &SafeWorkerManager{currentTaskIndex: workerCount - 1, tasks: tasks, errorLimit: errorLimit}
+	normalizedErrorLimit := errorLimit
+
+	if normalizedErrorLimit < 0 {
+		normalizedErrorLimit = 0
+	}
+
+	workerManager := &SafeWorkerManager{currentTaskIndex: workerCount - 1, tasks: tasks, errorLimit: normalizedErrorLimit}
 
 	for i := 0; i < workerCount; i++ {
 		if i+1 > len(tasks) {
@@ -69,14 +86,14 @@ func Run(tasks []Task, workerCount, errorLimit int) error {
 		}
 
 		workerManager.wg.Add(1)
-		go workerManager.handle(&i)
+		go workerManager.handle(&tasks[i])
 	}
 
 	workerManager.wg.Wait()
 
 	if workerManager.exceedErrorLimit {
 		return ErrErrorsLimitExceeded
-	} else {
-		return nil
 	}
+
+	return nil
 }
