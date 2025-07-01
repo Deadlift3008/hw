0bdafdb42a369cc822d3e8e4d869bf625348a7a6
deadlift3008 <deadlift3008@gmail.com>
2025-07-01 14:02:40 +0300

    hw05: add tests

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -14,6 +15,54 @@ import (
 
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
+
+	t.Run("should run tasks concurrently", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+		workersCount := 10
+
+		release := make(chan struct{})
+		var (
+			activeWorkers    int32
+			maxActiveWorkers int32
+		)
+
+		wg := sync.WaitGroup{}
+
+		for i := 0; i < tasksCount; i++ {
+			wg.Add(1)
+			tasks = append(tasks, func() error {
+				current := atomic.AddInt32(&activeWorkers, 1)
+
+				for {
+					oldMax := atomic.LoadInt32(&maxActiveWorkers)
+
+					if current > oldMax {
+						if atomic.CompareAndSwapInt32(&maxActiveWorkers, oldMax, current) {
+							break
+						}
+					} else {
+						break
+					}
+				}
+
+				<-release
+				atomic.AddInt32(&activeWorkers, -1)
+				wg.Done()
+
+				return nil
+			})
+		}
+
+		go Run(tasks, workersCount, 3)
+
+		time.Sleep(time.Millisecond * time.Duration(100))
+
+		close(release)
+		wg.Wait()
+
+		require.Equal(t, int32(workersCount), maxActiveWorkers)
+	})
 
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
@@ -38,33 +87,68 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
-	t.Run("tasks without errors", func(t *testing.T) {
-		tasksCount := 50
-		tasks := make([]Task, 0, tasksCount)
+	t.Run("edge cases", func(t *testing.T) {
+		t.Run("tasksCount less than workersCount", func(t *testing.T) {
+			tasksCount := 5
+			workersCount := 10
+			tasks := make([]Task, 0, tasksCount)
 
-		var runTasksCount int32
-		var sumTime time.Duration
+			var runTasksCount int32
 
-		for i := 0; i < tasksCount; i++ {
-			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
-			sumTime += taskSleep
+			for i := 0; i < tasksCount; i++ {
+				tasks = append(tasks, func() error {
+					time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+					atomic.AddInt32(&runTasksCount, 1)
+					return nil
+				})
+			}
 
-			tasks = append(tasks, func() error {
-				time.Sleep(taskSleep)
-				atomic.AddInt32(&runTasksCount, 1)
-				return nil
-			})
-		}
+			err := Run(tasks, workersCount, 1)
 
-		workersCount := 5
-		maxErrorsCount := 1
+			require.Nil(t, err, "Got error - %v", err)
+			require.Equal(t, runTasksCount, int32(tasksCount))
+		})
 
-		start := time.Now()
-		err := Run(tasks, workersCount, maxErrorsCount)
-		elapsedTime := time.Since(start)
-		require.NoError(t, err)
+		t.Run("workersCount less than 1", func(t *testing.T) {
+			tasksCount := 2
+			workersCount := 0
+			tasks := make([]Task, 0, tasksCount)
 
-		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were completed")
-		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+			var runTasksCount int32
+
+			for i := 0; i < tasksCount; i++ {
+				tasks = append(tasks, func() error {
+					time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+					atomic.AddInt32(&runTasksCount, 1)
+					return nil
+				})
+			}
+
+			err := Run(tasks, workersCount, 1)
+
+			require.Nil(t, err, "Got error - %v", err)
+			require.Equal(t, runTasksCount, int32(0))
+		})
+
+		t.Run("errorsLimit less than 1 must handled as 1", func(t *testing.T) {
+			tasksCount := 10
+			workersCount := 2
+			tasks := make([]Task, 0, tasksCount)
+
+			var runTasksCount int32
+
+			for i := 0; i < tasksCount; i++ {
+				tasks = append(tasks, func() error {
+					time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+					atomic.AddInt32(&runTasksCount, 1)
+					return nil
+				})
+			}
+
+			err := Run(tasks, workersCount, 0)
+
+			require.Nil(t, err, "Got error - %v", err)
+			require.Equal(t, runTasksCount, int32(10))
+		})
 	})
 }
