b7ef5e54660156a1915fad85d0fb3c95987ff798
deadlift3008 <deadlift3008@gmail.com>
2025-07-08 18:48:05 +0300

    hw06: WIP

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -18,58 +18,62 @@ type Result struct {
 	index int
 }
 
+func ExecuteStages(in In, done In, sortCh chan<- Result, stages ...Stage) {
+	wg := sync.WaitGroup{}
+	var i int
+
+	for value := range in {
+		wg.Add(1)
+		go func(i int) {
+			currentValue := value
+
+			for _, stage := range stages {
+				ch := make(chan interface{})
+				stageCh := stage(ch)
+
+				go func() {
+					ch <- currentValue
+				}()
+
+				currentValue = <-stageCh
+			}
+
+			sortCh <- Result{value: currentValue, index: i}
+			wg.Done()
+		}(i)
+		i++
+	}
+
+	wg.Wait()
+	close(sortCh)
+}
+
+func SortResults(sortCh <-chan Result, resultCh chan<- interface{}) {
+	results := []Result{}
+
+	for result := range sortCh {
+		results = append(results, result)
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].index < results[j].index
+	})
+
+	for _, res := range results {
+		resultCh <- res.value
+	}
+
+	close(resultCh)
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	resultCh := make(chan interface{})
-	orderCh := make(chan Result)
-	wg := sync.WaitGroup{}
+	sortCh := make(chan Result)
 
 	//TODO: добавить реагирование на done
-	go func() {
-		var i int
-
-		for value := range in {
-			wg.Add(1)
-			go func(i int) {
-				currentValue := value
-
-				for _, stage := range stages {
-					ch := make(chan interface{})
-					stageCh := stage(ch)
-
-					go func() {
-						ch <- currentValue
-					}()
-
-					currentValue = <-stageCh
-				}
-
-				orderCh <- Result{value: currentValue, index: i}
-				wg.Done()
-			}(i)
-			i++
-		}
-
-		wg.Wait()
-		close(orderCh)
-	}()
-
-	go func() {
-		results := []Result{}
-
-		for result := range orderCh {
-			results = append(results, result)
-		}
-
-		sort.Slice(results, func(i, j int) bool {
-			return results[i].index < results[j].index
-		})
-
-		for _, res := range results {
-			resultCh <- res.value
-		}
-
-		close(resultCh)
-	}()
+	// Написать общую функцию слушания канала select и переиспользовать на всех этапах чтения из каналов
+	go ExecuteStages(in, done, sortCh, stages...)
+	go SortResults(sortCh, resultCh)
 
 	return resultCh
 }
