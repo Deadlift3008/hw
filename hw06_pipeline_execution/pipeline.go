5b27722a5c6ac854ebc4c4ab2bc76a3d12d304ed
deadlift3008 <deadlift3008@gmail.com>
2025-07-24 20:13:21 +0300

    hw06: finished target solution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,15 +9,40 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
+	out := in
+
 	for _, stage := range stages {
 		tempCh := make(Bi)
 
-		go func(in In) {
-			// Слушаем in, отправляем в tempCh
-		}(in)
+		go func(ch In) {
+			defer func() {
+				go func() {
+					for {
+						_, ok := <-ch
 
-		in = stage(tempCh)
+						if !ok {
+							return
+						}
+					}
+				}()
+				close(tempCh)
+			}()
+
+			for {
+				select {
+				case msg, ok := <-ch:
+					if !ok {
+						return
+					}
+					tempCh <- msg
+				case <-done:
+					return
+				}
+			}
+		}(out)
+
+		out = stage(tempCh)
 	}
 
-	return in
+	return out
 }
